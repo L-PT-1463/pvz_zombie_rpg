8658785e396ebd55386a7e2e7177fc3858110891
L-PT-1463 <lucassoaresfonseca08@gmail.com>
2024-10-13 20:51:03 +0200

    Added - Various lists

---
+++ ideas_ressources_files/objects_list.cs
@@ -0,0 +1,21 @@
+//This is the complete list of objects for PvZ ZPG
+
+//object_tags
+    if (magnetisable = true){
+        string tag = "Weak to Magnetshroom's attacks";
+    }
+
+//template_category
+    
+    //template_armor
+        bool magnetisable = false;
+
+//armor_helmet
+
+//armor_shield
+
+//armor_umbrella
+
+//item_attack
+
+//item_passive
