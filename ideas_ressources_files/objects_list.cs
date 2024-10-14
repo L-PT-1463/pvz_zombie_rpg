25106556335d97fbff8259a48a011e61d90ed3df
L-PT-1463 <lucassoaresfonseca08@gmail.com>
2024-10-14 19:08:15 +0200

    Update - Objects_list

--- ideas_ressources_files/objects_list.cs
+++ ideas_ressources_files/objects_list.cs
@@ -4,6 +4,10 @@
     string  armor_helmet    = "Takes damage from all sources instead of your health but deosn't prevent status conditions.";
     string  armor_shield    = "Takes attacks from the front instead of your health and prevents status conditions.";
     string  armor_umbrella  = "Takes attacks from the top instead of your health and prevents status conditions.";
+    string  item_attack     = "An item designed to upgrade your attacks.";
+    string  item_passive    = "An item design to give you passive bonuses.";
+    string  dmg_bite        = "A bite based attack is affected by plants with eating based abilities."
+    string  dmg_strike      = "A non-bite based attack is unaffected by plants with eating based abilities.";
     bool    magnetisable    = "Weak to Magnetshroom's ability.";
 
 //template_category
@@ -15,8 +19,50 @@
         string  object_type     = ;
         bool    magnetisable    = false;
 
+    //template_item
+        string  description     = "";
+        string  dmg             = 0;
+        string  dmg_type        = ;
+        string  special         = "";
+        string  object_type     = ;   
+
 //armor
+    //cone
+        string  description     = "The most basic kind of armor.";
+        double  armor           = 10;
+        string  special         = "";
+        string  object_type     = armor_helmet;
+        bool    magnetisable    = false;
+
+    //bucket
+        string  description     = "A strong metalic helmet.";
+        double  armor           = 25;
+        string  special         = "";
+        string  object_type     = armor_helmet;
+        bool    magnetisable    = true;
+
+    //newspaper
+        string  description     = "A weak shield that buffs you upon destruction.";
+        double  armor           = 10;
+        string  special         = "Buffs the player's damage after it's destroyed.";
+        string  object_type     = armor_shield;
+        bool    magnetisable    = false;
 
 //item_attack
+    //bite
+        string  description     = "The standard zombie attack.";
+        string  dmg             = 1;
+        string  dmg_type        = dmg_bite;
+        string  special         = "";
+        string  object_type     = item_attack;
+
+    //riot_cane
+        string  description     = "A weak cane used to fend off plant riots.";
+        string  dmg             = 1;
+        string  dmg_type        = dmg_strike;
+        string  special         = "";
+        string  object_type     = item_attack;  
 
 //item_passive
+
+//gadget
