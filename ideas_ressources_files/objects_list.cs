e897f1d347fdc28a369f2d238fefb474993b722c
L-PT-1463 <lucassoaresfonseca08@gmail.com>
2024-10-14 09:01:21 +0200

    Update - Objects_list

--- ideas_ressources_files/objects_list.cs
+++ ideas_ressources_files/objects_list.cs
@@ -1,20 +1,21 @@
 //This is the complete list of objects for PvZ ZPG
 
 //object_tags
-    if (magnetisable = true){
-        string tag = "Weak to Magnetshroom's attacks";
-    }
+    string  armor_helmet    = "Takes damage from all sources instead of your health but deosn't prevent status conditions.";
+    string  armor_shield    = "Takes attacks from the front instead of your health and prevents status conditions.";
+    string  armor_umbrella  = "Takes attacks from the top instead of your health and prevents status conditions.";
+    bool    magnetisable    = "Weak to Magnetshroom's ability.";
 
 //template_category
     
     //template_armor
-        bool magnetisable = false;
+        string  description     = "";
+        double  armor           = 0;
+        string  special         = "";
+        string  object_type     = ;
+        bool    magnetisable    = false;
 
-//armor_helmet
-
-//armor_shield
-
-//armor_umbrella
+//armor
 
 //item_attack
 
