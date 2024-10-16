288b25848fe7c2fe6301a6a29ebaa9b74a32cf70
L-PT-1463 <lucassoaresfonseca08@gmail.com>
2024-10-16 09:10:08 +0200

    Update - Plants_list

--- ideas_ressources_files/plants_list.cs
+++ ideas_ressources_files/plants_list.cs
@@ -6,6 +6,7 @@
     string  lobber      = "Lobber plants attack at a range with lobbed projectiles.";
     string  melee       = "Melee plants attack at close proximity.";
     string  protector   = "Protector plants take hits for others; they must be attacked before the plants behind them.";
+    string  eaten       = "Eaten plants have special powers that specifically trigger on being eaten.";
     string  healer      = "Healer plants have powers allowing them to heal or buff other plants.";
     string  debuffer    = "Debuffer plants have powers that weaken the player through status conditions.";
     string  ground      = "Ground plants cannot be attacked by normal means but don't need to be defeated to complete a level. They do damage to zombies that attack a plant behind them.";
@@ -44,3 +45,19 @@
         string  range       = lane;
         string  special     = "Attacks twice";
         string  plant_type  = shooter;
+
+    //cardboard_sunflower
+        string  description = "";
+        double  hp          = 6;
+        double  dmg         = 0;
+        string  range       = eaten;
+        string  special     = "Eaten; Heals the player for 1hp.";
+        string  plant_type  = healer;
+
+    //cardboard_kernelpult
+        string  description = "";
+        double  hp          = 6;
+        double  dmg         = 0.5;
+        string  range       = lane;
+        string  special     = "Has a 1/4 chance of doing a debuff attack that blocks the target from playing its next turn.";
+        string  plant_type  = lobber;
